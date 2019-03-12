8333dcf770e765b0dc672c76da0b6cb8c9bddfd5
Miguel Campos <amodestduck@gmail.com>
2019-03-11 21:38:55 -0700

    Added Keyboard Class
    Feature.
    Implementing skeleton of keyboard reader.
    None.

---
+++ Assets/KeyboardReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void KeyPressedEventHandler(object sender, KeyPressedEventArgs e);
+
+public class KeyPressedEventArgs : System.EventArgs
+{
+    public KeyCode PressedKey { get; private set; }
+    public KeyPressedEventArgs(KeyCode pressedKey)
+    {
+        this.PressedKey = pressedKey;
+    }
+}
+
+public class KeyboardReader : MonoBehaviour
+{
+    public KeyPressedEventHandler KeyPressed;
+
+    public void OnKeyPressed(KeyPressedEventArgs e)
+    {
+        KeyPressed?.Invoke(this, e);
+    }
+}
