12d09f3835099b8c1e9dfb75bc32f122e8653f97
Miguel Campos <amodestduck@gmail.com>
2019-03-12 11:23:08 -0700

    Adding Game Manager Class
    Feature.
    Implementation of basic GameManager features.
    -
    -

--- Assets/Dictionary/WordDictionary.cs
+++ Assets/Dictionary/WordDictionary.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class WordDictionary : MonoBehaviour
+public class WordDictionary : MonoBehaviour, IEnumerable<KeyValuePair<string, string>>
 {
     Dictionary<string, string> dictionary;
 
@@ -15,38 +15,56 @@ public class WordDictionary : MonoBehaviour
         }
     }
 
-    public WordDictionary(int frequency, int maximumWords)
+    public WordDictionary(float frequency, int maximumWords)
     {
         dictionary = new Dictionary<string, string>();
         PopulateRandomly(frequency, maximumWords);
     }
+
 
     public KeyValuePair<string, string> GetRandom()
     {
         return dictionary.ElementAt(Random.Range(0, dictionary.Count));
     }
 
-    public void PopulateRandomly(int frequency, int maximumWords)
+    void PopulateRandomly(float frequency, int maximumWords)
     {
         string[] lines = TextLoader.LoadLines("dictionary");
         int i = 0;
         foreach(string line in lines)
         {
-            if(i >= maximumWords)
+            if (frequency >= Random.Range(0f, 1f))
+            {
+                string word, definition;
+                ParseWordDefPair(line, out word, out definition);
+                dictionary.Add(word, definition);
+                i++;
+            }
+
+            if (i > maximumWords)
             {
                 break;
             }
-
-            if (frequency/10f >= Random.Range(0f, 1f))
-            {
-                i++;
-                string[] pair = line.Split(':');
-                string word = pair[0].Trim(' ', '"');
-                string definition = pair[1].Trim(' ', '"', ',');
-                Debug.Log($"Word: {word}, Definition: {definition}");
-                dictionary.Add(word, definition);
-            }
-
         }
+    }
+
+    void ParseWordDefPair(string line, out string word, out string definition)
+    {
+        string[] pair = line.Split(':');
+        word = pair[0].Trim(' ', '"');
+        definition = pair[1].Trim(' ', '"', ',');
+    }
+
+    IEnumerator<KeyValuePair<string, string>> IEnumerable<KeyValuePair<string, string>>.GetEnumerator()
+    {
+        foreach(var entry in dictionary)
+        {
+            yield return entry;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new System.NotImplementedException();
     }
 }
---
+++ Assets/Managers/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] float frequency = .01f;
+    [SerializeField] int maximumWords = 100;
+    WordDictionary dictionary;
+
+    void Awake()
+    {
+        dictionary = new WordDictionary(frequency, maximumWords);
+        foreach(KeyValuePair<string, string> entry in dictionary)
+        {
+            Debug.Log(entry.Key);
+        }
+    }
+}
