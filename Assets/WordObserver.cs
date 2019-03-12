74f383f2225111eae4c73ec983c0db2f24dfb221
Miguel Campos <amodestduck@gmail.com>
2019-03-11 22:15:53 -0700

    Refactoring Keyboard Observer
    We needed a way to track current letters typed.
    New letters are turned into a full word.
    -
    -

---
+++ Assets/WordObserver.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void LetterReceivedEventHandler(object sender, LetterReceivedEventArgs e);
+
+public class LetterReceivedEventArgs : System.EventArgs
+{
+    public string Word { get; private set; }
+
+    public LetterReceivedEventArgs(List<KeyCode> letters)
+    {
+        StringBuilder builder = new StringBuilder(letters.Count);
+        foreach(KeyCode letter in letters)
+        {
+            builder.Append(letter);
+        }
+        Word = builder.ToString();
+    }
+}
+
+
+public class WordObserver : MonoBehaviour
+{
+    public LetterReceivedEventHandler LetterReceived;
+    List<KeyCode> keysPressed;
+
+    void Start()
+    {
+        keysPressed = new List<KeyCode>();
+        Keyboard.Instance.KeyPressed += LogKeyPress;
+    }
+
+    void OnLetterReceived(LetterReceivedEventArgs e)
+    {
+        LetterReceived?.Invoke(this, e);
+    }
+
+    public void LogKeyPress(object sender, KeyPressedEventArgs e)
+    {
+        keysPressed.Add(e.PressedKey);
+        OnLetterReceived(new LetterReceivedEventArgs(keysPressed));
+    }
+}
