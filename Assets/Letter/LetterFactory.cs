b8bba67e313d4b2b7764c44746edd1bbee3b42b2
Miguel Campos <amodestduck@gmail.com>
2019-03-11 22:57:39 -0700

    Adding Letter Spawning System
    Feature.
    Creation of skeleton & prefabs for letter spawning system.
    -
    -

---
+++ Assets/Letter/LetterFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Letter Factory", menuName = "Factories/Letter Factory")]
+public class LetterFactory : Factory<Letter>
+{ 
+}
---
+++ Assets/LetterSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterSpawner : MonoBehaviour
+{
+
+    public LetterFactory letterFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Letter letter = letterFactory.Get();
+            letter.gameObject.SetActive(true);
+        }
+    }
+}
