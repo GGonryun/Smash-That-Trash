a55f2ce52866b0e04dc341405e71218ca2f72563
Miguel Campos <amodestduck@gmail.com>
2019-03-12 11:45:43 -0700

    Adding Word Entries
    Feature.
    Implementation of Entry object and their data structure.
    -
    -

---
+++ Assets/Dictionary/Entries.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entries
+{
+    List<Entry> entries;
+
+    public Entries()
+    {
+        entries = new List<Entry>();
+    }
+
+    public void Add(Entry entry)
+    {
+        entries.Add(entry);
+    }
+
+    public float Accuracy()
+    {
+        int success = 0, total = 0;
+        foreach(var entry in entries)
+        {
+            total += entry.Word.Length;
+            success += entry.Score;
+        }
+        return success/(float)total;
+    }
+}
---
+++ Assets/Dictionary/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entry
+{
+    public string Word { get; private set; }
+    public string Definition { get; private set; }
+    public int Score { get; private set;}
+
+    public Entry(string word, string definition, int score)
+    {
+        Word = word;
+        Definition = definition;
+        Score = score;
+    }
+
+}
