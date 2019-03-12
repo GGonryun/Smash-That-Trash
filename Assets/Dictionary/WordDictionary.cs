472505b6f9ca912349843b58302c201f13a791c8
Miguel Campos <amodestduck@gmail.com>
2019-03-12 11:13:58 -0700

    Refactoring Word Dictionary
    Improvements
    Utility classes assist in reading and parsing data from text files.
    -
    -

---
+++ Assets/Dictionary/WordDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WordDictionary : MonoBehaviour
+{
+    Dictionary<string, string> dictionary;
+
+    public string this[string word]
+    {
+        get
+        {
+            return dictionary[word]; 
+        }
+    }
+
+    public WordDictionary(int frequency, int maximumWords)
+    {
+        dictionary = new Dictionary<string, string>();
+        PopulateRandomly(frequency, maximumWords);
+    }
+
+    public KeyValuePair<string, string> GetRandom()
+    {
+        return dictionary.ElementAt(Random.Range(0, dictionary.Count));
+    }
+
+    public void PopulateRandomly(int frequency, int maximumWords)
+    {
+        string[] lines = TextLoader.LoadLines("dictionary");
+        int i = 0;
+        foreach(string line in lines)
+        {
+            if(i >= maximumWords)
+            {
+                break;
+            }
+
+            if (frequency/10f >= Random.Range(0f, 1f))
+            {
+                i++;
+                string[] pair = line.Split(':');
+                string word = pair[0].Trim(' ', '"');
+                string definition = pair[1].Trim(' ', '"', ',');
+                Debug.Log($"Word: {word}, Definition: {definition}");
+                dictionary.Add(word, definition);
+            }
+
+        }
+    }
+}
---
+++ Assets/Utility/TextLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextLoader
+{
+    private static string Load(string filePath)
+    {
+        TextAsset textFile = Resources.Load(filePath) as TextAsset;
+        return textFile.text;
+    }
+
+    public static string[] LoadLines(string filePath)
+    {
+        string text = Load(filePath);
+        string[] lines = text.Split(new[] { "\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+        return lines;
+    }
+
+}
