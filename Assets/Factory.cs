cd11f61ba25e1c9cd90618f9e92a9023895aefc7
Miguel Campos <amodestduck@gmail.com>
2019-03-11 22:43:51 -0700

    Adding Factory Class
    Feature.
    Factory class allows for the use of object pooling.
    -
    -

---
+++ Assets/Factory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Factory<T> : ScriptableObject where T : MonoBehaviour
+{
+    public T template;
+
+    [System.NonSerialized] Stack<T> objectPool;
+    private Stack<T> ObjectPool
+    {
+        get
+        {
+            if(objectPool == null)
+            {
+                objectPool = new Stack<T>();
+            }
+            return objectPool;
+        }
+    }
+
+    public virtual T Get()
+    {
+
+        if (ObjectPool.Count > 0)
+        {
+            return ObjectPool.Pop();
+        }
+        else
+        {
+            return Instantiate(template) as T;
+        }
+    }
+
+
+    public virtual void Recycle(T obj)
+    {
+        ObjectPool.Push(obj);
+        obj.gameObject.SetActive(false);
+    }
+
+}
